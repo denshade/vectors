a61cc87a8a2efe4dcc803707c5345e06eca14c00
denshade <lieven.vaneeckhaute@gmail.com>
2020-11-05 22:19:26 +0100

    correct angles.

--- renderer.js
+++ renderer.js
@@ -1,27 +1,36 @@
-var renderVector = function (points) {
+var renderVector = function (cx, cy, middle) {
     var renderer = document.getElementById('renderer');
     var context = renderer.getContext('2d');
     context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(points[0], points[1]);
+    var x = middle + cx;
+    var y = middle - cy;
+    context.moveTo(middle, middle);
+    context.lineTo(x, y);
     context.stroke();
 };
+var clearScreen = function () {
+    var renderer = document.getElementById('renderer');
+    var context = renderer.getContext('2d');
+    context.clearRect(0, 0, 1000, 1000);
+};
 var lengthVect = function (points) {
-    return points[0] * points[0] + points[1] * points[1];
+    return Math.sqrt(points[0] * points[0] + points[1] * points[1]);
 };
 var loadVectors = function () {
+    clearScreen();
     var dataSource = document.getElementById('data');
     var text = dataSource.value;
     var resultingVector = [0, 0];
     text.split("\n").forEach(function (text) {
         if (text.split(",").length == 2) {
-            var currentVector = [parseFloat(text.split(",")[1]), parseFloat(text.split(",")[0])];
-            renderVector(currentVector);
-            resultingVector[0] += currentVector[0];
-            resultingVector[1] += currentVector[1];
+            var x = parseFloat(text.split(",")[0]);
+            var y = parseFloat(text.split(",")[1]);
+            renderVector(x, y, 500);
+            resultingVector[0] += x;
+            resultingVector[1] += y;
         }
     });
-    renderVector(resultingVector);
+    renderVector(resultingVector[0], resultingVector[1], 500);
     var results = document.getElementById('results');
     results.innerText = "Resulting vector: " + resultingVector;
     results.innerText = "Resulting length: " + lengthVect(resultingVector);
--- renderer.ts
+++ renderer.ts
@@ -1,30 +1,41 @@
-const renderVector = (points: number[]) =>
+const renderVector = (cx: number, cy: number, middle) =>
 {
     const renderer = document.getElementById('renderer') as HTMLCanvasElement;
     const context = renderer.getContext('2d') as CanvasRenderingContext2D;
     context.beginPath();
-    context.moveTo(0,0);
-    context.lineTo(points[0], points[1]);
+    const x = middle + cx;
+    const y = middle - cy;
+
+    context.moveTo(middle,middle);
+    context.lineTo(x, y);
     context.stroke();
 };
 
+const clearScreen = () => {
+    const renderer = document.getElementById('renderer') as HTMLCanvasElement;
+    const context = renderer.getContext('2d') as CanvasRenderingContext2D;
+    context.clearRect(0,0, 1000, 1000);
+};
+
 const lengthVect = (points) => {
-    return points[0] * points[0] + points[1]*points[1];
+    return Math.sqrt(points[0] * points[0] + points[1]*points[1]);
 }
 
 const loadVectors = () => {
+    clearScreen();
     const dataSource = document.getElementById('data') as HTMLTextAreaElement;
     const text = dataSource.value;
     let resultingVector = [0,0];
     text.split("\n").forEach((text) => {
         if (text.split(",").length == 2) {
-            const currentVector = [parseFloat(text.split(",")[1]), parseFloat(text.split(",")[0])];
-            renderVector(currentVector);
-            resultingVector[0] += currentVector[0];
-            resultingVector[1] += currentVector[1];
+            const x = parseFloat(text.split(",")[0]);
+            const y = parseFloat(text.split(",")[1]);
+            renderVector(x, y, 500);
+            resultingVector[0] += x;
+            resultingVector[1] += y;
         }
     });
-    renderVector(resultingVector);
+    renderVector(resultingVector[0], resultingVector[1], 500);
     const results = document.getElementById('results') as HTMLDivElement;
     results.innerText = "Resulting vector: " + resultingVector;
     results.innerText = "Resulting length: " + lengthVect(resultingVector);
